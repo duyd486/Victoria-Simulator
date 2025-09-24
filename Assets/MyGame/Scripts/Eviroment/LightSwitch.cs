11ab515fb3663092db65e23d5c7520f0a73ea9c3
duyd486 <duyd486@gmail.com>
2025-09-24 10:36:46 +0700

    update celling fan speed

--- Assets/MyGame/Scripts/Eviroment/LightSwitch.cs
+++ Assets/MyGame/Scripts/Eviroment/LightSwitch.cs
@@ -5,5 +5,14 @@ public class LightSwitch : MonoBehaviour, IInteractable
     public void Interact()
     {
         GameManager.Instance.TurnLightSwitch();
+        if (GameManager.Instance.lightRoom)
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+
+        }
     }
 }
